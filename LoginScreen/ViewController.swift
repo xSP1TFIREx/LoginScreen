8dd0d240f452b20a9d36ad160c526898498ad18b
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-11-04 12:57:34 +0300

    Initial Commit

---
+++ LoginScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by Дмитрий on 04.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
