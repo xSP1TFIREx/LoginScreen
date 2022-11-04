65dc4c53418c1e3c517ba88cd37c9c23041a9043
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-11-04 15:49:52 +0300

    Finale

--- LoginScreen/LoginViewController.swift
+++ LoginScreen/LoginViewController.swift
@@ -9,25 +9,28 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
     @IBAction func loginButtonPressed() {
-        
+        loginTF.text == "Anna" && passwordTF.text == "anna1995" ?
+        performSegue(withIdentifier: "GoToNextScreen", sender: self) :
+        showAlert(withTitle: "Error", andMessage: "Wrong username or password")
         
     }
+    
     @IBAction func forgotUNPressed() {
-        showAlert(withTitle: "Remainder", andMessage: "Your login is Anna")
-        
+        showAlert(withTitle: "Reminder", andMessage: "Your login is Anna")
     }
+    
     @IBAction func forgotPassPressed() {
-       showAlert(withTitle: "Remainder", andMessage: "Your password is anna1995")
-        
+        showAlert(withTitle: "Reminder", andMessage: "Your password is anna1995")
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let afterLoginVC = segue.destination as? AfterLoginViewController else { return }
-//        afterLoginVC.userName =
+        afterLoginVC.userName = loginTF.text ?? ""
+        afterLoginVC.view.backgroundColor = .yellow
     }
 }
 
