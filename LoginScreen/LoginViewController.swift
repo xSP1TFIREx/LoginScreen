fea768acc5f9c3c112be779c5a07b630cd174085
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-11-04 15:15:07 +0300

    Alert function has been added

--- LoginScreen/LoginViewController.swift
+++ LoginScreen/LoginViewController.swift
@@ -14,16 +14,29 @@ class LoginViewController: UIViewController {
 
     @IBAction func loginButtonPressed() {
         
+        
     }
     @IBAction func forgotUNPressed() {
+        showAlert(withTitle: "Remainder", andMessage: "Your login is Anna")
         
     }
     @IBAction func forgotPassPressed() {
+       showAlert(withTitle: "Remainder", andMessage: "Your password is anna1995")
         
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        <#code#>
+        guard let afterLoginVC = segue.destination as? AfterLoginViewController else { return }
+//        afterLoginVC.userName =
     }
 }
 
+//MARK: - AlertController
+
+extension LoginViewController {
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        present(alert, animated: true)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+    }
+}
