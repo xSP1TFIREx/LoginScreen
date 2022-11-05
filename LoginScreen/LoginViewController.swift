eee832286c5d66878cd27d15cf4a300cdfaeacef
Dmitry <107319523+xSP1TFIREx@users.noreply.github.com>
2022-11-05 20:50:16 +0300

    Major fixes

--- LoginScreen/LoginViewController.swift
+++ LoginScreen/LoginViewController.swift
@@ -1,22 +1,42 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     
     @IBOutlet weak var loginTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
+    
+    private let login = "Anna"
+    private let password = "anna2022"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
     }
- 
+    
+//    MARK: - Prepare function
+//    Подготовка данных к переносу
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = loginTF.text ?? ""
+    }
+        
+//    MARK: - Clearing fields
+//    Функция очистки полей
+    private func clearFields() {
+        loginTF.text = ""
+        passwordTF.text = ""
+    }
+//    MARK: - Clearing Password field
+    private func clearPassField() {
+        passwordTF.text = ""
+    }
 //    MARK: - Buttons Action
 //    Нажатие кнопки логин
     @IBAction func loginButtonPressed() {
-        loginTF.text == "Anna" && passwordTF.text == "anna2022" ?
+        loginTF.text == login && passwordTF.text == password ?
         performSegue(withIdentifier: "GoToNextScreen", sender: self) :
-        showAlert(withTitle: "Error", andMessage: "Wrong username or password")
+        showAlert(withTitle: "Error", andMessage: "Wrong username or password"); clearPassField()
     }
     
 //    Нажатие кнопки забыл логин
@@ -29,26 +49,11 @@ class LoginViewController: UIViewController {
         showAlert(withTitle: "Reminder", andMessage: "Your password is anna2022")
     }
     
-//    MARK: - Prepare function
-//    Подготовка данных к переносу
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let WelcomeVC = segue.destination as? WelcomeViewController else { return }
-        WelcomeVC.userName = loginTF.text ?? ""
-        WelcomeVC.view.backgroundColor = .yellow
-    }
-    
 //    MARK: - Unwind
 //    "Перемотка" - набор действий при возврате к начальному экрану
     @IBAction func unwind(for segue: UIStoryboardSegue) {
-        guard let afterLoginVC = segue.source as? WelcomeViewController else { return }
+        let afterLoginVC = segue.source as? WelcomeViewController
         clearFields()
-    }
-    
-//    MARK: - Clearing fields
-//    Функция очистки полей
-    private func clearFields() {
-        loginTF.text = ""
-        passwordTF.text = ""
     }
 }
 
